6c52b00fd8680b55092e90dac813c17b7a0303bb
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2023-11-07 13:25:59 +0800

    corrections

--- src/phase_vs_time_all2ps.sh
+++ src/phase_vs_time_all2ps.sh
@@ -2,14 +2,21 @@
 
 mkdir -p images
 
-for ant in 
+ant=1
+while [[ $ant -le 256 ]];
 do
    ant_str="'antennae($ant)'"
    ant_psfile=`echo $ant | awk '{printf("vis_antenna_%03d.ps",$1);}'`   
+   ant_pdffile=`echo $ant | awk '{printf("vis_antenna_%03d.pdf",$1);}'`   
    
-   echo "uvplt device='/cps' vis=chan_410_20231105T02????.uv  axis=time,phase stokes=xx,yy yrange=-200,200 select=${ant_str} nxy=3,3"
-   uvplt device='/cps' vis=chan_410_20231105T02????.uv  axis=time,phase stokes=xx,yy yrange=-200,200 select=${ant_str} nxy=3,3
+   echo "uvplt device='/cps' vis=chan_410_20231105T0[2,3,4]????.uv  axis=time,phase stokes=xx,yy yrange=-200,200 select=${ant_str} nxy=3,3"
+   uvplt device='/cps' vis=chan_410_20231105T0[2,3,4]????.uv  axis=time,phase stokes=xx,yy yrange=-200,200 select=${ant_str} nxy=3,3
    
-   echo "mv pgplot.ps images/${ant_psfile}"
-   mv pgplot.ps images/${ant_psfile}
+#   echo "mv pgplot.ps images/${ant_psfile}"
+#   mv pgplot.ps images/${ant_psfile}
+
+   echo "ps2pdf pgplot.ps images/${ant_pdffile}"
+   ps2pdf pgplot.ps images/${ant_pdffile}
+   
+   ant=$(($ant+1))
 done
