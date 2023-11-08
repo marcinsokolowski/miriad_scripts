097b8aa92513f2b574ae54edab66626122ee5d05
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2023-11-08 15:42:18 +0800

    correction

--- src/phase_vs_time_all2ps.sh
+++ src/phase_vs_time_all2ps.sh
@@ -9,14 +9,18 @@ do
    ant_psfile=`echo $ant | awk '{printf("vis_antenna_%03d.ps",$1);}'`   
    ant_pdffile=`echo $ant | awk '{printf("vis_antenna_%03d.pdf",$1);}'`   
    
-   echo "uvplt device='/cps' vis=chan_410_20231105T0[2,3,4]????.uv  axis=time,phase stokes=xx,yy yrange=-200,200 select=${ant_str} nxy=3,3"
-   uvplt device='/cps' vis=chan_410_20231105T0[2,3,4]????.uv  axis=time,phase stokes=xx,yy yrange=-200,200 select=${ant_str} nxy=3,3
+   if [[ -s images/${ant_pdffile} ]]; then
+      echo "PROGRESS : file images/${ant_pdffile} already exists -> skipped ant = $ant"
+   else
+      echo "uvplt device='/cps' vis=chan_410_20231105T0[2,3,4]????.uv  axis=time,phase stokes=xx,yy yrange=-200,200 select=${ant_str} nxy=3,3"
+      uvplt device='/cps' vis=chan_410_20231105T0[2,3,4]????.uv  axis=time,phase stokes=xx,yy yrange=-200,200 select=${ant_str} nxy=3,3
    
 #   echo "mv pgplot.ps images/${ant_psfile}"
 #   mv pgplot.ps images/${ant_psfile}
 
-   echo "ps2pdf pgplot.ps images/${ant_pdffile}"
-   ps2pdf pgplot.ps images/${ant_pdffile}
+      echo "ps2pdf pgplot.ps images/${ant_pdffile}"
+      ps2pdf pgplot.ps images/${ant_pdffile}
+   fi
    
    ant=$(($ant+1))
 done
