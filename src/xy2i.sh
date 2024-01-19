fce2512a753449fcbeb9fc3783fe9bfc9657c574
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2024-01-19 15:19:50 +0800

    correction

--- src/xy2i.sh
+++ src/xy2i.sh
@@ -11,10 +11,14 @@ do
    yy_file=${b}_YY.fits
    i_file=${b}_I.fits
    
-   echo "calcfits_bg ${xx_file} B ${yy_file} ${i_file}"
-   calcfits_bg ${xx_file} B ${yy_file} ${i_file}
+   if [[ -s ${i_file} ]]; then
+      echo "File ${i_file} already exists -> skipped"
+   else   
+      echo "calcfits_bg ${xx_file} B ${yy_file} ${i_file}"
+      calcfits_bg ${xx_file} B ${yy_file} ${i_file}
 
-   ls ${i_file} > tmp_new_list_to_process
-   echo "python $image_path tmp_new_list_to_process  --outdir images_i/ --ext=png  --fits_list=tmp_new_list_to_process --out_image_type=\"png\""
-   python $image_path tmp_new_list_to_process  --outdir images_i/ --ext=png  --fits_list=tmp_new_list_to_process --out_image_type="png" 
+      ls ${i_file} > tmp_new_list_to_process
+      echo "python $image_path tmp_new_list_to_process  --outdir images_i/ --ext=png  --fits_list=tmp_new_list_to_process --out_image_type=\"png\""
+      python $image_path tmp_new_list_to_process  --outdir images_i/ --ext=png  --fits_list=tmp_new_list_to_process --out_image_type="png" 
+   fi
 done
